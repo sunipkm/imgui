55553d2db74919c91fd7f538e32a9835500bcfce
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2021-07-27 17:04:37 -0400

    Upgraded to WIP 1.84 and added Windows NT support

--- src/main_dx12.cpp
+++ src/main_dx12.cpp
@@ -6,7 +6,7 @@
 // This is because we need ImTextureID to carry a 64-bit value and by default ImTextureID is defined as void*.
 // This define is set in the example .vcxproj file and need to be replicated in your app or by adding it to your imconfig.h file.
 
-#include "imgui/"
+#include "imgui/imgui.h"
 #include "backend/imgui_impl_win32.h"
 #include "backend/imgui_impl_dx12.h"
 #include <d3d12.h>
