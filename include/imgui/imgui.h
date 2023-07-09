f422572482f7845b56bec1e21bf67895ad37f763
Sunip K. Mukherjee <sunipkmukherjee@gmail.com>
2023-07-09 21:43:45 +0530

    ImGui::SelectableInput() added

--- include/imgui/imgui.h
+++ include/imgui/imgui.h
@@ -616,7 +616,7 @@ namespace ImGui
     // - Neighbors selectable extend their highlight bounds in order to leave no gap between them. This is so a series of selected Selectable appear contiguous.
     IMGUI_API bool          Selectable(const char* label, bool selected = false, ImGuiSelectableFlags flags = 0, const ImVec2& size = ImVec2(0, 0)); // "bool selected" carry the selection state (read-only). Selectable() is clicked is returns true so you can modify your selection state. size.x==0.0: use remaining width, size.x>0.0: specify width. size.y==0.0: use label height, size.y>0.0: specify height
     IMGUI_API bool          Selectable(const char* label, bool* p_selected, ImGuiSelectableFlags flags = 0, const ImVec2& size = ImVec2(0, 0));      // "bool* p_selected" point to the selection state (read-write), as a convenient helper.
-
+    IMGUI_API bool          SelectableInput(const char* str_id, bool selected, ImGuiSelectableFlags flags = 0, char* buf = NULL, size_t buf_size = 0);
     // Widgets: List Boxes
     // - This is essentially a thin wrapper to using BeginChild/EndChild with some stylistic changes.
     // - The BeginListBox()/EndListBox() api allows you to manage your contents and selection state however you want it, by creating e.g. Selectable() or any items.
